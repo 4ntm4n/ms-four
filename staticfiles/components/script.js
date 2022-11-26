a4e3a06b22c8ee36073e8ac5a5d9effc0682aa09
Anton A <anton.askling@gmail.com>
2022-11-26 22:27:33 +0100

    build out slider for 'home'

---
+++ staticfiles/components/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    options={height: 500,}
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
