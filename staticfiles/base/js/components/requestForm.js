7aedef9ad3fe0f0abc26c4738d030c413c318a95
Anton A <anton.askling@gmail.com>
2022-12-08 20:30:48 +0100

    extend send request form validation with javascript

--- staticfiles/base/js/components/requestForm.js
+++ staticfiles/base/js/components/requestForm.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // find date from and date to input fields
+    const dateFromInput = document.getElementById('id_date_from');
+    const dateToInput = document.getElementById('id_date_to');
+
+    //if jS loaded, set date to disabled as default state.
+    dateToInput.setAttribute("disabled", true);
+    
     const datepickerStart = document.getElementById('id_date_from');
     M.Datepicker.init(datepickerStart, {
         format: "yyyy-mm-dd",
@@ -8,22 +15,33 @@ document.addEventListener('DOMContentLoaded', function() {
         maxDate: new Date(),
         maxYear: new Date().getFullYear(), 
 
+
     });
 
-    const dateInput = document.getElementById('id_date_from');
-    dateInput.addEventListener('change', (event) => {
-    selectedDate = event.target.value;
+    dateFromInput.addEventListener('change', (event) => {
+        const selectedDate = event.target.value;
+        date1 = new Date(selectedDate);
+        
+        
 
         const datepickerEnd = document.getElementById('id_date_to');
         M.Datepicker.init(datepickerEnd, {
-            format: "yyyy-mm-dd",
+            format: 'yyyy-mm-dd',
             firstDay: 1,
-            yearRange: 70,
+            yearRange: 20,
             minDate: new Date(selectedDate),
             maxDate: new Date(),
             maxYear: new Date().getFullYear(), 
         });
-      
+        
+        dateToInput.disabled=false;
     });
 
+   // if from date is NaN, tell user to fill in that date first.
+    dateToInput.addEventListener('click', (event) => {
+        if (dateFromInput.value === ""){
+            alert("enter from date first")
+        };       
+    })
+   
 });
