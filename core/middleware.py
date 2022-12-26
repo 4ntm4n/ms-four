331bc8518579074e165e3f27d323d6f7ccb0cda8
A Askling <anton.askling@gmail.com>
2022-12-26 12:59:12 +0100

    fix: try to specify amiresponsive as allowed for iframe embedding

--- core/middleware.py
+++ core/middleware.py
@@ -2,5 +2,5 @@ from django.utils.deprecation import MiddlewareMixin
 
 class XFrameOptionsMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        response['x-frame-options'] = 'sameorigin'
+        response['x-frame-options'] = 'allow-from https://ui.dev/amiresponsive'
         return response
